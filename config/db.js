ccfd85bd9b79a2e64afb74dec8f581d1ccf9c1cf
madhumithamandava06 <madhumithamandava06@gmail.com>
2026-07-06 10:00:30 +0530

    Updated

--- config/db.js
+++ config/db.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const connectdb = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("✅ MongoDB Connected Successfully");
+        console.log("MongoDB Connected Successfully");
     } catch (err) {
-        console.log("❌ MongoDB Connection Failed");
+        console.log("MongoDB Connection Failed");
         console.log(err.message);
         process.exit(1);
     }
